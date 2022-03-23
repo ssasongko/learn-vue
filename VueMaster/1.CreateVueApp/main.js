576ddee6071af1e86ac945d004c4c56e7bbf09a8
Nur Sasongko <kokonur1020@gmail.com>
2022-03-23 15:45:25 +0700

    Initial commit

---
+++ VueMaster/1.CreateVueApp/main.js
@@ -0,0 +1,9 @@
+// create vue app
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            description: 'Using socks while its night make you warmer'
+        }
+    }
+})
